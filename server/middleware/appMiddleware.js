cc741f8b531e67f2bd420dfc1bec0e4e8f887e89
kareemhamdy500 <kareemhamdy500@gmail.com>
2019-10-21 17:18:58 +0200

    fix error handler

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -16,5 +16,7 @@ exports.globalMiddleware = (app)=> {
 
 // eslint-disable-next-line no-unused-vars
 exports.errMiddleware = (err,req , res , next)=>{
-      res.json({errMSG: err.message});
+      console.log(err.status)
+      const status = err.status || 400
+      res.status(status).json({errMSG: err.message});
 }
